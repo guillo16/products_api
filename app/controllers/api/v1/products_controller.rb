17b1187273175e33287d6cf46edff00809298aa6
guillermo varela <guillovarela16@gmail.com>
2021-05-17 11:18:04 -0300

    Change product index

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ProductsController < Api::V1::BaseController
   def index
     @products = Product.all
 
-    render json: @products
+    render json: @products, only: %w[title photo description price]
   end
 end
